cbb70b4dfcf3f81cb9c67854220a7885aac10e0a
Anton Sushnikov <resonans93@gmail.com>
2024-12-20 15:26:04 +0500

    feat: write code to sort

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -418,8 +418,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const sortedArr = [...arr];
+  for (let i = 0; i <= sortedArr.length; i += 1) {
+    for (let j = 0; j < sortedArr.length; j += 1) {
+      if (sortedArr[j] >= sortedArr[j + 1]) {
+        const item = sortedArr[j];
+        sortedArr[j] = sortedArr[j + 1];
+        sortedArr[j + 1] = item;
+      }
+    }
+  }
+  return sortedArr;
 }
 
 /**
